6a1867176e681226a1146a05be0ee5c68ca00b96
cindyfalencia <cindyfalenciai@gmail.com>
2025-02-26 11:10:02 +0800

    Avatar props

--- app/components/avatar/avtar3d.js
+++ app/components/avatar/avtar3d.js
@@ -96,9 +96,45 @@ export function Avatar3d({ userId, ...props }) {
         <group name="Avatar">
           <skinnedMesh
             name="avaturn_body"
-            geometry={nodes.avaturn_body?.geometry}
+            geometry={nodes.avaturn_body.geometry}
             material={materials.avaturn_body_material}
-            skeleton={nodes.avaturn_body?.skeleton}
+            skeleton={nodes.avaturn_body.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_glasses_0"
+            geometry={nodes.avaturn_glasses_0.geometry}
+            material={materials.avaturn_glasses_0_material}
+            skeleton={nodes.avaturn_glasses_0.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_glasses_1"
+            geometry={nodes.avaturn_glasses_1.geometry}
+            material={materials.avaturn_glasses_1_material}
+            skeleton={nodes.avaturn_glasses_1.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_hair_0"
+            geometry={nodes.avaturn_hair_0.geometry}
+            material={materials.avaturn_hair_0_material}
+            skeleton={nodes.avaturn_hair_0.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_hair_1"
+            geometry={nodes.avaturn_hair_1.geometry}
+            material={materials.avaturn_hair_1_material}
+            skeleton={nodes.avaturn_hair_1.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_look_0"
+            geometry={nodes.avaturn_look_0.geometry}
+            material={materials.avaturn_look_0_material}
+            skeleton={nodes.avaturn_look_0.skeleton}
+          />
+          <skinnedMesh
+            name="avaturn_shoes_0"
+            geometry={nodes.avaturn_shoes_0.geometry}
+            material={materials.avaturn_shoes_0_material}
+            skeleton={nodes.avaturn_shoes_0.skeleton}
           />
           <primitive object={nodes.Hips} />
         </group>
